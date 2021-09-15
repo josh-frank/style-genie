c78f4d2981cef6c734d4b9c8cd60d28233d46f82
josh-frank <eurosewusa@gmail.com>
2021-09-15 15:07:51 -0400

    experimenting with css parsing

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cssParser = require( "css" );
 exports.parse = css => {
     const parsedCss = cssParser.parse( css );
     return parsedCss.stylesheet.rules.reduce( ( result, rule ) => {
-        return rule.selectors ? [ ...result, [ rule.selectors, rule.declarations ] ] : result;
-    }, [] );
+        rule.selectors?.forEach( selector => result[ selector ] = ( result[ selector ] || "" ) + rule.declarations.reduce( ( result, { property, value } ) => property && value ? [ ...result, `${ property }: ${ value };` ] : result, [] ).join( " " ) );
+        return result;
+        // return rule.selectors ? [ ...result, [ rule.selectors, rule.declarations ] ] : result;
+    }, {} );
 };
