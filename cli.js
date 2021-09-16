223472b163b7e35e386e236a3778d303c8bb13eb
josh-frank <eurosewusa@gmail.com>
2021-09-16 16:06:06 -0400

    working cli

--- cli.js
+++ cli.js
@@ -1,25 +1,54 @@
 "use strict";
 
-const styleGenie = require( "." );
-
 /* eslint no-process-exit: 0 */
 /* eslint global-require: 0 */
 
-const parseFile = ( error, data ) => {
-    if ( error ) {
-        console.error( error );
-        return;
-    }
-    console.log( styleGenie.parse( data ) );
+const fs = require( "fs" );
+const styleGenie = require( "." );
+
+const respondToError = error => { if ( error ) { console.log( error ); return } };
+
+const jsify = string => string.split( /\W/g ).map( word => word.charAt( 0 ).toUpperCase() + word.slice( 1 ) ).join( "" );
+
+const writeToFile = ( error, file ) => {
+  respondToError( error );
+  const parsedFile = styleGenie.parse( file );
+  const styledComponentsFromFile = Object.keys( parsedFile ).reduce( ( result, selector ) => [ ...result, `
+export const ${ jsify( selector ) } = styled.div\`${ parsedFile[ selector ] }\`;\n
+  ` ], [ 'import styled from "styled-components"\n' ] );
+  fs.writeFile( `${ new Date().getTime() }-${ args[ 1 ].replace( /\.\w+$/g, "" ) }.js`, styledComponentsFromFile.join( "" ), respondToError );
+};
+
+const testInConsole = ( error, file ) => {
+  respondToError( error );
+  console.log( styleGenie.parse( file ) );
+};
+
+const printHelpMessage = () => {
+  console.clear();
+  console.log( [
+    "yarn cli --w <filename>\t\tReads a file, parses it, and writes the result locally",
+    "yarn cli --t <filename>\t\tReads a file, parses it, and logs the result in the console",
+    "yarn cli --h\t\t\tPrints this help message"
+  ].join( "\n\n" ) );
 };
 
 if ( require.main === module ) {
   let sliceN = 1;
   if ( process.argv.indexOf( module.filename ) > -1 || require( "path" ).basename( process.argv[ 1 ] ) === "parse-paths-from-svg" ) sliceN = 2;
   const args = process.argv.slice( sliceN, process.argv.length );
-  if ( args.length === 0 ) {
-    console.error( "Error" );
-    process.exit( 1 );
+  switch ( args[ 0 ] ) {
+    case "--w":
+      fs.readFile( args[ 1 ], "utf8", writeToFile );
+      break;
+    case "--t":
+      fs.readFile( args[ 1 ], "utf8" , testInConsole );
+      break;
+    case "--h":
+      printHelpMessage();
+      break;
+    default:
+      console.error( "Error" );
+      process.exit( 1 );
   }
-  require( "fs" ).readFile( args[ 0 ], "utf8" , parseFile );
 }
