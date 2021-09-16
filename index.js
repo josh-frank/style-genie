7e22a91f893c1bca92f3bf1f43bef30957657ef6
josh-frank <eurosewusa@gmail.com>
2021-09-16 14:33:08 -0400

    tweaking the way rules/selectors merge

--- index.js
+++ index.js
@@ -13,44 +13,32 @@ const joinRules = ( rule, completeStyle, selector ) => completeStyle[ selector ]
 
 const addBlockToObject = ( selector, stylesObject, blockToAdd ) => stylesObject[ selector ] = ( stylesObject[ selector ] || "" ) + blockToAdd;
 
-// exports.parse = css => {
-//     const parsedCss = cssParser.parse( css );
-//     const stylesObject = parsedCss.stylesheet.rules.reduce( ( completeStyle, rule ) => {
-//         rule.selectors?.forEach( selector => joinRules( rule, completeStyle, selector ) );
-//         return completeStyle;
-//     }, {} );
-//     parsedCss.stylesheet.rules.filter( rule => rule.media ).forEach( rule => {
-//         const mediaObject = rule.rules.reduce( ( completeMediaRule, mediaRule ) => {
-//             mediaRule.selectors?.forEach( selector => joinRules( mediaRule, completeMediaRule, selector ) );
-//             return completeMediaRule;
-//         }, {} );
-//         Object.keys( mediaObject ).forEach( mediaSelector => {
-//             stylesObject[ mediaSelector ] = ( stylesObject[ mediaSelector ] || "" ) + ` @media ${ rule.media } { ${ mediaObject[ mediaSelector ] } }`;
-//         } );
-//     } );
-//     return stylesObject;
-// };
+const mergeWithMatchingSelectors = ( stylesObject, selector ) => {
+    for ( let otherSelector of Object.keys( stylesObject ).filter( otherSelector => otherSelector !== selector ) ) {
+        if ( otherSelector.match( `${ selector }[:\[\\s]+` ) ) {
+            addBlockToObject( selector, stylesObject, ` ${ otherSelector.replace( selector, "&" ) } { ${ stylesObject[ otherSelector ]} }` );
+            delete stylesObject[ otherSelector ];
+        }
+    }
+};
 
 exports.parse = css => {
     const parsedCss = require( "css" ).parse( css );
+    const mediaRules = parsedCss.stylesheet.rules.filter( rule => rule.media );
     const stylesObject = parsedCss.stylesheet.rules.reduce( ( completeStyle, rule ) => {
         rule.selectors?.forEach( selector => joinRules( rule, completeStyle, selector ) );
         return completeStyle;
     }, {} );
-    parsedCss.stylesheet.rules.filter( rule => rule.media ).forEach( rule => {
+    mediaRules.forEach( rule => {
         const mediaObject = rule.rules.reduce( ( completeMediaRule, mediaRule ) => {
             mediaRule.selectors?.forEach( selector => joinRules( mediaRule, completeMediaRule, selector ) );
             return completeMediaRule;
         }, {} );
-        Object.keys( mediaObject ).forEach( mediaSelector => addBlockToObject( mediaSelector, stylesObject, ` @media ${ rule.media } { ${ mediaObject[ mediaSelector ] } }` ) );
+        Object.keys( mediaObject ).forEach( mediaSelector => {
+            mergeWithMatchingSelectors( mediaObject, mediaSelector );
+            addBlockToObject( mediaSelector, stylesObject, ` @media ${ rule.media } { ${ mediaObject[ mediaSelector ] } }` )
+        } );
     } );
-    Object.keys( stylesObject ).forEach( selector => {
-        for ( let otherSelector of Object.keys( stylesObject ).filter( otherSelector => otherSelector !== selector ) ) {
-            if ( otherSelector.match( `${ selector }\\s+` ) ) {
-                addBlockToObject( selector, stylesObject, ` ${ otherSelector.replace( selector, "&" ) } { ${ stylesObject[ otherSelector ]} }` );
-                delete stylesObject[ otherSelector ];
-            }
-        }
-    } );
+    Object.keys( stylesObject ).forEach( selector => mergeWithMatchingSelectors( stylesObject, selector ) );
     return stylesObject;
 };
