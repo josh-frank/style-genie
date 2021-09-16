c2ff977452d0ac72c197b7cd1d18806540d73c83
Josh Frank <eurosewusa@gmail.com>
2021-09-16 09:20:03 -0400

    added css library, toArray method

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const cssParser = require( "css" );
 
+const toArray = parsedCss => {
+    return Object.keys( parsedCss ).reduce( ( result, selector ) => {
+        return [ ...result, [ selector, `${ selector } {
+            ${ parsedCss[ selector ] }
+        }` ] ];
+    }, [] );
+};
+
 exports.parse = css => {
     const parsedCss = cssParser.parse( css );
-    return parsedCss.stylesheet.rules.reduce( ( result, rule ) => {
-        rule.selectors?.forEach( selector => result[ selector ] = ( result[ selector ] || "" ) + rule.declarations.reduce( ( result, { property, value } ) => property && value ? [ ...result, `${ property }: ${ value };` ] : result, [] ).join( " " ) );
-        return result;
-        // return rule.selectors ? [ ...result, [ rule.selectors, rule.declarations ] ] : result;
+    const stylesObject = parsedCss.stylesheet.rules.reduce( ( completeStyle, rule ) => {
+        rule.selectors?.forEach( selector => completeStyle[ selector ] = ( completeStyle[ selector ] || "" ) + rule.declarations.reduce( ( completeRule, { property, value } ) => property && value ? [ ...completeRule, `${ property }: ${ value };` ] : completeRule, [] ).join( " " ) );
+        return completeStyle;
     }, {} );
+    parsedCss.stylesheet.rules.filter( rule => rule.media ).forEach( rule => {
+        const mediaObject = rule.rules.reduce( ( completeMediaRule, mediaRule ) => {
+            mediaRule.selectors?.forEach( selector => completeMediaRule[ selector ] = ( completeMediaRule[ selector ] || "" ) + mediaRule.declarations.reduce( ( completeRule, { property, value } ) => property && value ? [ ...completeRule, `${ property }: ${ value };` ] : completeRule, [] ).join( " " ) );
+            return completeMediaRule;
+        }, {} );
+        stylesObject[ `@media ${ rule.media }` ] = mediaObject;
+    } );
+    return toArray( stylesObject );
 };
